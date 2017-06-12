4ed44df193254ea210af480a83c073f0dc6fbf52
Will Xavier <will.lbx@hotmail.com>
2017-06-12 17:20:24 -0400

    Adjacenciais

---
+++ Magoxter/Laboratorio/Aula-Pratica/Adjacenciais.c
@@ -0,0 +1,25 @@
+// Programa: adjacencias.c
+// Programador: Willian Xavier
+// Data: 10/09/2016
+// Este programa lê o número de vértices de um dado digrafo, e as
+// arestas (dadas por pares ordenados, o par 0 0 indica que final das
+// arestas) e computa a matriz de adjacências do grafo (n x n).
+// Declaração das bibliotecas utilizadas
+#include<stdio.h> // scanf, printf
+// Declaração das constantes
+#define MAX 100   // limita do número de linhas na matriz
+// Declaração de tipos
+typedef int matriz[MAX][MAX];
+
+// início da função principal
+int main(void) {
+// declaração das variáveis locais
+
+// pré: UmInt, G | para i,j em {1,...,vertices} && (i,j) é uma aresta de G
+
+
+   return 0;
+
+// pós: para todo i em {1,...,n}: para todo j em {1,...,r}: (a[i][j] == 1
+//      && (i, j) é uma aresta de G) || (c[i][j] == 0)
+} // fim da função principal
